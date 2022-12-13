230803bf914a5263dfdca55fcb2f80496b172aab
azmithabet <azmy-rady@elarabygroup.com>
2022-12-14 00:24:19 +0200

    add read excels file

--- app/src/main/java/com/azmiradi/churchapp/all_applications/AllApplicationsScreen.kt
+++ app/src/main/java/com/azmiradi/churchapp/all_applications/AllApplicationsScreen.kt
@@ -2,15 +2,14 @@ package com.azmiradi.churchapp.all_applications
 
 import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
@@ -26,7 +25,11 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.azmiradi.churchapp.NavigationDestination.APPLICATION_DETAILS
 import com.azmiradi.churchapp.ProgressBar
+import com.azmiradi.churchapp.RealPathUtil
+import com.azmiradi.churchapp.application_details.saveBitmap
 import com.azmiradi.churchapp.exel.common.ExcelUtils
+import java.io.File
+import java.util.Calendar
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -39,6 +42,16 @@ fun AllApplicationsScreen(
     val state = viewModel.stateApplications.value
     val context = LocalContext.current
 
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { result ->
+            result?.let {
+              //  val item = RealPathUtil.getRealPath(context, result)
+              //  println("items: "+ item)
+            }
+
+        }
+
     ProgressBar(isShow = state.isLoading)
     if (state.error.isNotEmpty()) {
         LaunchedEffect(Unit) {
@@ -50,10 +63,11 @@ fun AllApplicationsScreen(
     }
 
     LaunchedEffect(Unit) {
-        //  viewModel.getApplications()
-        applicationsList.addAll(ExcelUtils.readFromExcelWorkbook(context,"1670959220829.xls"))
-        println("applicationsList:: "+ applicationsList.size)
+        viewModel.getApplications()
+        // applicationsList.addAll(ExcelUtils.readFromExcelWorkbook(context,"1670959220829.xls"))
+        // println("applicationsList:: "+ applicationsList.size)
     }
+
 
     state.data?.let {
         LaunchedEffect(Unit) {
@@ -81,7 +95,42 @@ fun AllApplicationsScreen(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier.padding(top = 5.dp, end = 10.dp, start = 10.dp, bottom = 10.dp)
         ) {
+            item {
+                Row(Modifier.fillMaxWidth()) {
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(end = 10.dp),
+                        onClick = {
+                            launcher.launch("application/vnd.ms-excel")
+                        }) {
+                        Text(
+                            text = "استيراد XLS", fontSize = 16.sp
+                        )
+                    }
 
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(start = 10.dp),
+                        onClick = {
+                            val list = ExcelUtils.exportDataIntoWorkbook(
+                                context,
+                                Calendar.getInstance().timeInMillis.toString(),
+                                applicationsList
+                            )
+                            if (list) {
+                                Toast.makeText(context, "تم حفظ الملف", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(context, "فشل حفظ الملف", Toast.LENGTH_LONG).show()
+                            }
+                        }) {
+                        Text(
+                            text = "تصدير XLS", fontSize = 16.sp
+                        )
+                    }
+                }
+            }
             items(applicationsList) {
                 ApplicationItem(it) {
                     onNavigate(APPLICATION_DETAILS, it.nationalID ?: "")
